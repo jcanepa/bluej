fcda3a2108fc1b20f2d43c915b79230cdf713f18
J. Canepa <julian.l.canepa@gmail.com>
2021-04-12 16:26:40 -0700

    Add comments to cart.

--- projects/chapter08/zuul/Cart.java
+++ projects/chapter08/zuul/Cart.java
@@ -10,34 +10,47 @@ public class Cart
 {
     private final ArrayList<Item> items;
 
-    /**
-     * Constructor for objects of class Cart
-     */
     public Cart()
     {
         items = new ArrayList<Item>();
     }
     
+    /**
+     * Return the number of items in the cart.
+     */
     public int getItemCount()
     {
         return items.size();
     }
     
+    /**
+     * Add an item to the cart.
+     */
     public void addItem(Item item)
     {
         items.add(item);
     }
     
+    /**
+     * Remove an item from the cart at a given index.
+     * Return the item that was removed.
+     */
     public Item removeItem(int index)
     {
         return items.remove(index);
     }
     
+    /**
+     * Get the item at a given  index.
+     */
     public Item getItem(int index)
     {
         return items.get(index);
     }
     
+    /**
+     * Return the sum total of cart items.
+     */
     public int getTotalPrice()
     {
         return items.stream()
