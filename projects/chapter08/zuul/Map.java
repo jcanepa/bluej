2eee8fa20baaf7462445cda887e6de5819417478
J. Canepa <julian.l.canepa@gmail.com>
2021-04-30 18:39:07 -0700

    Finish transporter room class functionality.

--- projects/chapter08/zuul/Map.java
+++ projects/chapter08/zuul/Map.java
@@ -12,91 +12,53 @@ public class Map
 {
     private Room entrance;
     private Room[][] layout;
-    private TransporterRoom transporter;
 
     public Map()
-    {       
-        createRooms();
-    }
-    
-    /**
-     * Create all the rooms and link their exits together.
-     * Place items around the map.
-     */
-    private void createRooms()
     {
-        final var appliances = new Room("Appliances (north west)");
-        final var athletics = new Room("Athletic clothing & sports (north center)");
-        final var audio = new Room("Audio department (north east)");
-        final var bedroom = new Room("Bedroom furnishings (mid west)");
-        final var bath = new Room("Bathroom & fixures (mid center)");
-        final var baby = new Room("Baby, children & toys (mid east)");
-        final var checkout = new Room("The register queue. Ready to checkout?");
-        final var camping = new Room("Camping supplies (south center)");
-        final var computers = new Room("Computer and electronics (south east)");
+        layout = new Room[][] {
+            new Room[] {
+                new Room("Appliances"),
+                new Room("Audio"),
+                new TransporterRoom(),
+            },
+            new Room[] {
+                new Room("Batteries"),
+                new Room("Bedroom"),
+                new Room("Books"),
+            },
+            new Room[] {
+                new Room("Checkout"),
+                new Room("Camping"),
+                new Room("Computers"),
+            }
+        };
         
-        Room firedoor = new Room("Fire door is a one-way exit. You are locked out!");
-
-        // initialise room's exits
-        appliances.setExit("east", athletics);
-        appliances.setExit("south", bedroom);
+        connectRooms();
         
-        athletics.setExit("east", audio);
-        athletics.setExit("south", bath);
-        athletics.setExit("west", appliances);
-        athletics.setExit("north", firedoor);
+        setEntrance(layout[2][0]);
         
-        audio.setExit("south", baby);
-        audio.setExit("west", athletics);
+        if (layout[0][2] instanceof TransporterRoom) {
+            var transport = (TransporterRoom) layout[0][2];
+            transport.setDestination(entrance);
+        }
         
-        bedroom.setExit("north", appliances);
-        bedroom.setExit("east", bath);
-        bedroom.setExit("south", checkout);
-        
-        bath.setExit("north", athletics);
-        bath.setExit("east", baby);
-        bath.setExit("south", camping);
-        bath.setExit("west", bedroom);
-        
-        baby.setExit("north", audio);
-        baby.setExit("south", computers);
-        baby.setExit("west", bath);
-        
-        checkout.setExit("north", bedroom);
-        checkout.setExit("east", camping);
-        
-        camping.setExit("north", bath);
-        camping.setExit("east", computers);
-        camping.setExit("west", checkout);
-        
-        computers.setExit("north", baby);
-        computers.setExit("west", camping);
-        
-        // add items to the map
-        checkout.addItem("Macbook", 1500);
-        checkout.addItem("Macbook Pro", 2500);
-        checkout.addItem("M1 Mac Mini", 1650);
-        checkout.addItem("5k Monitor", 650);
-        checkout.addItem("Thunderbolt cable", 25);
-        
-        // add gift cards to the room
-        checkout.addItem("Gift card", -100);
-        
-        // create the store's departments and initialize their exits
-        transporter = new TransporterRoom(checkout);
-        transporter.setExit("south", audio);
-        audio.setExit("north", transporter);
-        
-        entrance = checkout;
+        addItems();
     }
     
     /**
-     * Create a new room with a given description
+     * Print the map.
      * @param description Details of the room including name and purpose.
      */
-    private void createRoom(String description)
+    public void printLayout()
     {
-        new Room(description);
+        for (Room[] row : layout) {
+
+            for (Room room : row) {
+                System.out.print(room);
+            }
+
+            System.out.println();
+        }
     }
 
     /**
@@ -113,5 +75,136 @@ public class Map
     public Room getEntrance()
     {
         return entrance;
+    }
+    
+    /**
+     * Build exits between rooms on the map's layout.
+     */
+    private void connectRooms()
+    {
+        for (int row = 0; row < layout.length; row++) {
+
+            for (int col = 0; col < layout[row].length; col++) {
+
+                Room room = layout[row][col];
+
+                /*
+                 * Add a north exit to the room above
+                 */
+                if (!isOnNorthWall(room)) {
+
+                    room.setExit(
+                        "north", 
+                        layout[row - 1][col]
+                    );
+                }
+
+                /*
+                 * Add an east exit to the room on the right.
+                 */
+                if (!isOnEastWall(room)) {
+
+                    room.setExit(
+                        "east", 
+                        layout[row][col + 1]
+                    );
+                }
+
+                /*
+                 * Add a south exit to the room below.
+                 */
+                if (!isOnSouthWall(room)) {
+
+                    room.setExit(
+                        "south", 
+                        layout[row + 1][col]
+                    );
+                }
+
+                /*
+                 * Add a west exit to the room to the left.
+                 */
+                if (!isOnWestWall(room)) {
+
+                    room.setExit(
+                        "west", 
+                        layout[row][col - 1]
+                    );
+                }
+            }
+        }
+    }
+    
+    /**
+     * Deturmine if a given room is in the first row.
+     */
+    private boolean isOnNorthWall(Room room)
+    {
+        for (Room northRoom : layout[0]) {
+            if (northRoom == room) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+        /**
+     * Deturmine if a given room is in the last row.
+     */
+    private boolean isOnSouthWall(Room room)
+    {
+        for (Room southRoom : layout[2]) {
+            if (southRoom == room) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+        /**
+     * Deturmine if a given room is in the first row.
+     */
+    private boolean isOnEastWall(Room room)
+    {
+        for (int i = 0; i < 3; i++) {
+            if (layout[i][2] == room) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+        /**
+     * Deturmine if a given room is in the first row.
+     */
+    private boolean isOnWestWall(Room room)
+    {
+        for (int i = 0; i < 3; i++) {
+            if (layout[i][0] == room) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    
+    /**
+     * Add various items around the map.
+     */
+    private void addItems()
+    {
+        // add items to the map
+        entrance.addItem("Macbook", 1500);
+        entrance.addItem("Macbook Pro", 2500);
+        entrance.addItem("M1 Mac Mini", 1650);
+        entrance.addItem("5k Monitor", 650);
+        entrance.addItem("Thunderbolt cable", 25);
+        
+        // add gift cards to the room
+        entrance.addItem("Gift card", -100);
+        
     }
 }
